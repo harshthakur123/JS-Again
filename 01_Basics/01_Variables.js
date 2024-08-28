0a73d3205307f996dd5a4df05e6b3184d5de6636
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-28 17:47:15 +0530

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 632001
+let email = "harsh123@gmail.com"
+var password = "12345"
+city = "Raipur"
+
+// accountId=2 not allowed
+email="hkt123@gmail.com"
+password="346275"
+city="Pune"
+let state;
+
+console.log(accountId);
+console.table([accountId,email,password,city,state]) 
+
+/*
+If no value is assigned to a variable, its value
+undefined by default
+*/
+
+/*
+Prefer not to use var 
+because of issue in block and functional scope
+*/
