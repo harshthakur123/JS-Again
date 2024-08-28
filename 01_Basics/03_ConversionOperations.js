404dea59eae48edf8418e20988e05cfbc32507ab
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-28 18:35:09 +0530

    Conversions

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,23 @@
+// let score = "hhhk"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" -> 33
+// "333abc" -> NaN
+// true -> 1 ; false -> 0
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" -> false
+// "HARSH" -> true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
