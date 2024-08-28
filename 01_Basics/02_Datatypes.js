111f4600926ccd63a43023d40daad27699670ab6
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-28 17:47:27 +0530

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+// console.log(3+3); console.log("Hello"); // follow clean code strategy
+
+let name="Harsh" //String
+let age=23   //Number
+let isLoggedIn=false // boolean
+
+// DATATYPES
+
+//number => 2 to the power 53  , bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value/ representation of empty value / its an object
+//undefined => value is not assigned till now / its a type
+//symbol => unique
+// object 
+
+console.log(typeof "Harsh");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
