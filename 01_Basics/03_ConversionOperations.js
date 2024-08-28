eb0749db03af831c3e879f894849afe5a7b5b73f
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-28 20:32:51 +0530

    Conversions

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -16,8 +16,54 @@
 
 // "" -> false
 // "HARSH" -> true
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+/*****************************OPERATIONS********************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+
+let str1 = "Hello "
+let str2 = "Harsh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+// console.log(num1);
+
+let gameCounter=100
+console.log(gameCounter++);
+console.log(gameCounter);
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
 
