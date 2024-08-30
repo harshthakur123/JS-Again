8b4fe54b10ece7eaa341c46b1b5766ab8662c44b
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-30 19:59:47 +0530

    Math And Numbers

---
+++ 01_Basics/07_Nums_And_Math.js
@@ -0,0 +1,43 @@
+// const score = 400
+// console.log(typeof score);
+
+// const balance = new Number(600)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const bal = 23.46474
+// console.log(bal.toPrecision(3));
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+//-------------------------------MATH---------------------------------
+// console.log(Math);
+// console.log(Math.abs(-4)); //convert neg to pos value
+// console.log(Math.abs(4));
+// console.log(Math.round(4.676));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(2,3,57,2,235,2,-9));
+// console.log(Math.max(2,3,57,2,235,2,-9));
+
+console.log(Math.random());
+console.log(Math.random()*10);  // it gives value between 0 and 1
+console.log((Math.random()*10)+1); //never be zero
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
