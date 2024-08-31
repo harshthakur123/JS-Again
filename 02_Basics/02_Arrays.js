b3cfb522cb5471cd40d36ceaf9de94c5a770bd37
harshthakur123 <thakurharshsingh99@gmail.com>
2024-08-31 18:22:47 +0530

    Arrays Part 2

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,33 @@
+// const heroes1  = ["Thor","Ironmann","Hulk"]
+// const heroes2 = ["Batman","Flash","Superman"]
+
+// // heroes1.push(heroes2) //add 2nd array as an elemnet in 1st array
+// // console.log(heroes1);
+// // console.log(heroes1[3][1]);
+
+// const all_heroes = heroes1.concat(heroes2) // limitation -> can merge only two
+// //arrays at a time
+// console.log(all_heroes); // merge two arrays and returns a new array
+// const all_heroes2 = [...heroes1,...heroes2]
+// console.log(all_heroes2);
+
+
+// const arr1 = [35,21,1,24,[3,4,5],8,[4,6,[3,5,6]]]
+// const arr2 = arr1.flat(Infinity) // infinity includes all depths
+// // can give depth also in args upto what depth should it spread
+// console.log(arr2);
+
+// console.log(Array.isArray("Harsh"));
+// console.log(Array.from("Harsh"));
+// console.log(Array.from({name : "Harsh"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+Array.from
+
+
+
+
