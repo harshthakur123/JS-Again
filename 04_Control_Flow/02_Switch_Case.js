472795e5aaf7230b38e60363781271d377564115
harshthakur123 <thakurharshsingh99@gmail.com>
2024-09-04 19:20:59 +0530

    Switch-Case

---
+++ 04_Control_Flow/02_Switch_Case.js
@@ -0,0 +1,45 @@
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard ){
+//     console.log("Allow to buy course");
+    
+// }
+
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("user logged in");
+// }
+
+// switch(key){
+//    case value:
+//         break;
+
+//     default :
+//         brreak;
+// }
+
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("Mar");
+        break;
+    case "apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default");
+        
+        break;
+} 
