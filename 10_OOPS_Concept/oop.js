c2570b867c853fcfeeae9997e8ff95f583c0b8e2
harshthakur123 <thakurharshsingh99@gmail.com>
2024-09-18 12:43:22 +0530

    OOPs Concept

---
+++ 10_OOPS_Concept/oop.js
@@ -0,0 +1,30 @@
+const user = {
+    username : "Harsh",
+    loginCount : 8,
+    signedin : true,
+
+    getUserDetails : function(){
+        console.log(`Got user details : ${this.username}`);
+        console.log(this);
+        
+    }
+}
+
+console.log(user.username);
+// console.log(user.getUserDetails());
+console.log(this);
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this;
+}
+
+const userOne = new User("Harsh",9,true)
+const userTwo = new User("pk",10,false)
+
+
+console.log(userOne);
+console.log(userTwo);
